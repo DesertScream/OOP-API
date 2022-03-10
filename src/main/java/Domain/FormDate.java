5e2aaab38dcabfec2d8faa72d9724190ddc62761
DesertScream <www.anton.dzuin@gmail.com>
2022-03-11 00:49:55 +0500

    initial commit

---
+++ src/main/java/Domain/FormDate.java
@@ -0,0 +1,7 @@
+package Domain;
+
+public class FormDate {
+    public int day;
+    public int month;
+    public int year;
+}
---
+++ src/main/java/Domain/Post.java
@@ -0,0 +1,14 @@
+package Domain;
+
+public class Post {
+    public Post() {
+        birthday = new FormDate();
+    }
+    public String name;
+    public String patronymic;
+    public String surname;
+    public String passport;
+    public String phone;
+    public boolean subscription;
+    public FormDate birthday;
+}
